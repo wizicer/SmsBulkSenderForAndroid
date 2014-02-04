17c1b573e7e7c1260fa9d56b5c72c3fd52d4a294
Icer <liangshuangde@163.com>
2014-02-04 22:05:15 +0800

    initial check in, can bulk send already

---
+++ SmsBulkSenderForAndroid/frmMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SmsBulkSenderForAndroid
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstSms = new System.Windows.Forms.ListView();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lblConnectionStatus = new System.Windows.Forms.Label();
+            this.colheadPhonenumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colheadName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colheadContent = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSend = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lstSms
+            // 
+            this.lstSms.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstSms.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colheadPhonenumber,
+            this.colheadName,
+            this.colheadContent,
+            this.columnHeader1});
+            this.lstSms.FullRowSelect = true;
+            this.lstSms.Location = new System.Drawing.Point(12, 44);
+            this.lstSms.Name = "lstSms";
+            this.lstSms.Size = new System.Drawing.Size(642, 380);
+            this.lstSms.TabIndex = 0;
+            this.lstSms.UseCompatibleStateImageBehavior = false;
+            this.lstSms.View = System.Windows.Forms.View.Details;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImport.Location = new System.Drawing.Point(579, 12);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lblConnectionStatus
+            // 
+            this.lblConnectionStatus.AutoSize = true;
+            this.lblConnectionStatus.Location = new System.Drawing.Point(12, 17);
+            this.lblConnectionStatus.Name = "lblConnectionStatus";
+            this.lblConnectionStatus.Size = new System.Drawing.Size(77, 12);
+            this.lblConnectionStatus.TabIndex = 2;
+            this.lblConnectionStatus.Text = "Disconnected";
+            // 
+            // colheadPhonenumber
+            // 
+            this.colheadPhonenumber.Text = "Phonenumber";
+            this.colheadPhonenumber.Width = 88;
+            // 
+            // colheadName
+            // 
+            this.colheadName.Text = "Name";
+            this.colheadName.Width = 69;
+            // 
+            // colheadContent
+            // 
+            this.colheadContent.Text = "Content";
+            this.colheadContent.Width = 457;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(579, 431);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Status";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 466);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblConnectionStatus);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.lstSms);
+            this.Name = "frmMain";
+            this.Text = "Sms Bulk Sender";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.Resize += new System.EventHandler(this.frmMain_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstSms;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label lblConnectionStatus;
+        private System.Windows.Forms.ColumnHeader colheadPhonenumber;
+        private System.Windows.Forms.ColumnHeader colheadName;
+        private System.Windows.Forms.ColumnHeader colheadContent;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ SmsBulkSenderForAndroid/frmMain.cs
@@ -0,0 +1,211 @@
+﻿namespace SmsBulkSenderForAndroid
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using System.Web;
+    using System.Windows.Forms;
+    using Timer = System.Threading.Timer;
+
+    public partial class frmMain : Form
+    {
+        private string username = "icer";
+        private string password = "icericer";
+        private Timer timer;
+        private string host = "http://192.168.1.127:25118";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void timerRefreshConnectionStatus(object state)
+        {
+            this.Invoke((Action)(() => this.lblConnectionStatus.Text = "Trying To Connect"));
+            bool isConnected = false;
+            try
+            {
+                var page = GetPage(host + "/newmessage?");
+                isConnected = page.Contains(@"<form name=""sendform"" method=""GET"" action=""/newmessage"">");
+            }
+            catch (TimeoutException)
+            {
+                isConnected = false;
+            }
+
+            if (isConnected)
+            {
+                this.Invoke((Action)(() => SetConnected()));
+            }
+            else
+            {
+                this.Invoke((Action)(() => SetDisconnected()));
+            }
+        }
+
+        private void SetConnected()
+        {
+            this.lblConnectionStatus.Text = "Connected";
+            this.btnImport.Enabled = true;
+            this.btnSend.Enabled = true;
+        }
+
+        private void SetDisconnected()
+        {
+            this.lblConnectionStatus.Text = "Disconnected";
+            this.btnImport.Enabled = false;
+            this.btnSend.Enabled = false;
+        }
+
+        private string GetPage(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+                client.Headers[HttpRequestHeader.Authorization] = "Basic " + credentials;
+                client.Encoding = Encoding.UTF8;
+                try
+                {
+                    string page = client.DownloadString(url);
+                    return page;
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Status == WebExceptionStatus.ProtocolError)
+                    {
+                        throw new TimeoutException();
+                    }
+
+                    WebResponse resp = ex.Response;
+                    using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                    {
+                        var r = sr.ReadToEnd();
+                        throw new Exception(r, ex);
+                    }
+                }
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog()
+            {
+                DefaultExt = ".txt",
+                Filter = "(Tab delimit text)*.txt|*.txt",
+            };
+
+            if (dlg.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+            }
+
+            var inputFile = dlg.FileName;
+
+            if (string.IsNullOrEmpty(inputFile)) return;
+
+            lstSms.Items.Clear();
+            this.btnImport.Enabled = false;
+            this.btnImport.Text = "Importing";
+            Application.DoEvents();
+            try
+            {
+                var cpage = this.GetPage(host + "/addressbook?");
+                var regex = new Regex(@"addNumber\('(?<name>.*)','(?<number>\+?\d{4,18})'\)");
+                var contacts = regex.Matches(cpage)
+                    .OfType<Match>()
+                    .Select(m => new { name = m.Groups["name"].Value, number = m.Groups["number"].Value })
+                    .GroupBy(o => o.number)
+                    .Select(g => g.First())
+                    .ToDictionary(o => o.number, o => o.name);
+
+                var ins = ReadAllLines(inputFile)
+                    .Select(ln => ln.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries))
+                    .Where(s => s.Length == 2)
+                    .Select(s => new { address = s[0], body = s[1] })
+                    .ToArray();
+
+                this.lstSms.Items.AddRange(ins
+                    .Select(s =>
+                    {
+                        var lvt = new ListViewItem(s.address);
+                        var name = contacts.ContainsKey(s.address) ? contacts[s.address] : string.Empty;
+                        lvt.SubItems.Add(name);
+                        lvt.SubItems.Add(s.body);
+                        lvt.SubItems.Add(string.Empty);
+                        return lvt;
+                    })
+                    .ToArray());
+            }
+            finally
+            {
+                this.btnImport.Enabled = true;
+                this.btnImport.Text = "Import";
+            }
+        }
+
+        private static IEnumerable<string> ReadAllLines(string filename)
+        {
+            using (var fs = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var sr = new StreamReader(fs, Encoding.Default, true))
+            {
+                while (!sr.EndOfStream)
+                {
+                    yield return sr.ReadLine();
+                }
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            timer = new Timer(timerRefreshConnectionStatus, null, 500, 10000);
+            this.SetDisconnected();
+            ResizeList();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var msg = string.Format(@"{0} message(s) to send, are you sure?", this.lstSms.Items.Count);
+
+            if (MessageBox.Show(msg, "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.No)
+            {
+                return;
+            }
+
+            var items = this.lstSms.Items
+                .OfType<ListViewItem>()
+                .Select(lvt => new
+                {
+                    number = lvt.Text.Trim(),
+                    content = lvt.SubItems[2].Text.Trim(),
+                    reference = lvt,
+                });
+            foreach (var item in items)
+            {
+                var url = string.Format(
+                    host + "/newmessage?empf={0}&type=send&text={1}",
+                    item.number,
+                    HttpUtility.UrlEncode(item.content, Encoding.UTF8));
+                var response = this.GetPage(url);
+                item.reference.SubItems[3].Text = "Sent";
+                Thread.Sleep(100);
+            }
+        }
+
+        private void frmMain_Resize(object sender, EventArgs e)
+        {
+            ResizeList();
+        }
+
+        private void ResizeList()
+        {
+            var otherWidth = this.lstSms.Columns.OfType<ColumnHeader>().Sum(h => h.Width) - colheadContent.Width;
+            var scrollbarWidth = 40;
+            colheadContent.Width = this.lstSms.Width - otherWidth - scrollbarWidth;
+        }
+    }
+}
